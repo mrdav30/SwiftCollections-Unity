ab7a0fa1568c5c62f57e0926cc7e8b57fc4fe4ab
mrdav30 <david.oravsky@gmail.com>
2026-06-21 23:06:00 -0400

    chore: bump SwiftCollections to v5.1.0
    - fixed sample path

--- com.mrdav30.swiftcollections.fixedmathsharp/Editor/Utility/GitDependencyInstaller.cs
+++ com.mrdav30.swiftcollections.fixedmathsharp/Editor/Utility/GitDependencyInstaller.cs
@@ -46,7 +46,7 @@ namespace SwiftCollections.Editor
             new(
                 "com.mrdav30.fixedmathsharp",
                 "https://github.com/mrdav30/FixedMathSharp-Unity.git?path=/com.mrdav30.fixedmathsharp",
-                "v5.0.2"
+                "v5.0.3"
             )
         };
 
